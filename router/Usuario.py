425371e923820f287960743d2f84c55f7e3c0dc6
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-08-24 21:17:56 -0500

    router usuario logica

--- router/Usuario.py
+++ router/Usuario.py
@@ -1,14 +1,22 @@
-from fastapi import APIRouter , Cookie
-
+from fastapi import APIRouter , Cookie , Response
+from  pydantic import BaseModel, Field , EmailStr
 userRouter = APIRouter(prefix="/user", tags=['user'])
 
+datos=[
+    {"personaID":"password"}
+    ]
+class Login(BaseModel):
+    email: str
+    password: str
 @userRouter.get('/me')
 async def user_perfil():
     return {"user":"perfil"}
 
 @userRouter.post('/login')
-async def user_login():
-    pass
+async def user_login(usuarioLogin:Login, response:Response):
+    for data in datos.dict:
+        if data['personaID'] == usuarioLogin.password:
+            return data['personaID']
 
 @userRouter.post('/register')
 async def user_register():
